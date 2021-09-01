b1d7b967aed4652f0b830c06db2d8839f14669dc
PradeepKK13 <51114651+PradeepKK13@users.noreply.github.com>
2021-09-01 16:29:12 +0400

    Update GooglePlus.java

--- src/android/GooglePlus.java
+++ src/android/GooglePlus.java
@@ -145,7 +145,9 @@ implements GoogleApiClient.OnConnectionFailedListener {
     private void createGoogleApiClient() {
         //create builder we can manipulate
         GoogleSignInOptions.Builder optionsBuilder = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-	.requestScopes(new Scope(DriveScopes.DRIVE))
+	.requestScopes(new Scope("https://www.googleapis.com/auth/drive"), new Scope("https://www.googleapis.com/auth/drive.readonly"))
+	.requestScopes(new Scope("https://www.googleapis.com/auth/drive.readonly"))
+	.requestScopes(new Scope("https://www.googleapis.com/auth/calendar"))
 	.requestEmail();
 	    //.requestScopes(new Scope("https://www.googleapis.com/auth/drive"), new Scope("https://www.googleapis.com/auth/drive.readonly"))
 	    //.requestScopes(new Scope("https://www.googleapis.com/auth/drive.readonly"))
